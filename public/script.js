8ecdd59cd585e9b589e527fd06d75792b415b941
J3SS3HVA <jesse.leeuwerink@hva.nl>
2024-03-21 13:38:23 +0100

    ver 0.5
    eerste post gemaakt.
    leestlijst

--- public/script.js
+++ public/script.js
@@ -5,7 +5,6 @@ searchBtn = body.querySelector(".search-box"),
 modeSwitch = body.querySelector(".toggle-switch"),
 modeText = body.querySelector(".mode-text");
 
-
 toggle.addEventListener("click" , () =>{
     sidebar.classList.toggle("close");
 })
@@ -25,3 +24,21 @@ modeSwitch.addEventListener("click" , () =>{
     }
 });
 
+const listpopup = body.querySelector(".item-added") 
+const closelist = body.querySelector(".item-added button") 
+
+closelist.addEventListener("click" , () =>{
+    listpopup.classList.add("closed");
+})
+
+function setButtonText() {
+    const button = document.querySelector('.add');
+    const itemId = button.getAttribute('data-item-id'); // Haal het item ID op uit het attribuut
+  
+    // Controleer of het item ID overeenkomt met een item in de leeslijst
+    if (leeslijst[itemId]) {
+      button.innerText = "Toegevoegd";
+    }
+  }
+  
+  window.onload = setButtonText;
--- server.js
+++ server.js
@@ -23,6 +23,10 @@ app.set('views', './views')
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
+// array & objects
+let leeslijst = {}
+
+
 // 2. routes
 app.get('/', function(request, response) {
   fetchJson(apiItem).then((items) => {
@@ -69,20 +73,49 @@ app.get('/detail/:id', function(request, response){
         response.render('detail', {
             
             items: items.data/*hier zeg ik dat iedereen getoond moet worden*/
-
+          
         });
     })
 })
 
 app.post('/detail/:id', function(request, response){
-    fetchJson(apiItem + '?filter={"id":' + request.params.id + '}').then((items) => {
-        response.render('detail', {
-        
-            items: items.data/*hier zeg ik dat iedereen getoond moet worden*/
+     // Er is nog geen afhandeling van POST, redirect naar GET op /
+  const id = request.params.id;
+  
 
-         });
-    })
-})
+  // Voeg het bericht toe aan de messages array
+  leeslijst[id] = true;
+  console.log(leeslijst)
+
+  // Redirect naar de GET route voor de specifieke persoon met het bijgewerkte bericht
+  // '/person/' + id zorgt ervoor dat hij redirect naar waar je de message hebt aangemaakt. 
+  response.redirect(303, '/detail/' + id + '?added=true');
+});
+  
+
+app.get('/leeslijst', function(request, response) {
+  fetchJson(apiItem).then((items) => {
+      
+      let itemsOpLeeslijst = []
+      // Loop door alle items.data,
+      // Kijk of het id van dat item in de leeslijst staat
+      // Voeg in dat geval dat item toe aan itemsOpLeeslijst
+      items.data.forEach(function(item) {
+        if (leeslijst[item.id]) {
+          itemsOpLeeslijst.push(item)
+        }
+      })
+  
+      if (itemsOpLeeslijst.length) {
+          response.render('leeslijst', {
+              items: itemsOpLeeslijst
+          });
+      } else {
+          console.error("Invalid or unexpected API response format");
+          response.status(500).send("Internal Server Error");
+      }
+  });
+});
 
 // 3. Start de webserver
 
